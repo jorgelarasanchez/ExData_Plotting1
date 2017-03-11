188f29fb383c6496520a3f3fdf0eaed8e38c0679
jorgelarasanchez <larasan@hotmail.com>
2017-03-11 19:17:40 +0100

    Add files and plots

---
+++ plot1.R
@@ -0,0 +1,33 @@
+# File URL to download
+
+fileURL <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+dataFileZip <- "./household_power_consumption.zip"
+
+# Download the file
+if (file.exists(dataFileZip) == FALSE) {
+  download.file(fileURL,destfile=dataFileZip ,method="curl")
+  # Unzip the file
+  unzip(zipfile=dataFileZip ,exdir=".")
+}
+
+dataFile <- "./household_power_consumption.txt"
+
+# We take only with field "date" 1 and 2 of February of 2007
+
+dataFile_12 <- grep("^[1,2]/2/2007",readLines(dataFile),value=TRUE)
+
+# Read the data
+
+data <- read.table(text=dataFile_12, sep = ';', col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), na.strings = '?')
+
+# open device
+
+png(filename = './plot1.png', width = 480, height = 480, units='px')
+
+# plot
+
+with(data, hist(Global_active_power, xlab = 'Global Active Power (kilowatts)', main = 'Global Active Power', col = 'red'))
+
+# close device
+
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,48 @@
+# File URL to download
+
+fileURL <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+dataFileZip <- "./household_power_consumption.zip"
+
+# Download the file
+if (file.exists(dataFileZip) == FALSE) {
+  download.file(fileURL,destfile=dataFileZip ,method="curl")
+  # Unzip the file
+  unzip(zipfile=dataFileZip ,exdir=".")
+}
+
+dataFile <- "./household_power_consumption.txt"
+
+# We take only with field "date" 1 and 2 of February of 2007
+dataFile_12 <- grep("^[1,2]/2/2007",readLines(dataFile),value=TRUE)
+
+# Read the data
+
+data <- read.table(text=dataFile_12, sep = ';', col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), na.strings = '?')
+
+## set time variable
+
+SetTime <-strptime(paste(data$Date, data$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
+data <- cbind(SetTime, data)
+
+# open device
+
+png(filename = './plot4.png', width = 480, height = 480, units='px')
+
+# plot 
+
+Sys.setlocale(category = "LC_ALL", locale = "english")
+columnlines <- c("black", "red", "blue")
+labels <- c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+
+par(mfrow = c(2, 2))
+plot(data$SetTime, data$Global_active_power, col = columnlines[1], xlab = '', ylab = 'Global Active Power', type = 'l')
+plot(data$SetTime, data$Voltage, col = columnlines[1], xlab = 'datetime', ylab = 'Voltage', type = 'l')
+plot(data$SetTime, data$Sub_metering_1, col = columnlines[1], xlab = '', ylab = 'Energy sub metering', type = 'l')
+lines(data$SetTime, data$Sub_metering_2, col = columnlines[2])
+lines(data$SetTime, data$Sub_metering_3, col = columnlines[3])
+legend('topright', bty="n", col =columnlines, legend = labels, lwd = 1)
+plot(data$SetTime, data$Global_reactive_power,col = columnlines[1], xlab = 'datetime', ylab = 'Global_reactive_power', type = 'l')
+
+# close device
+
+dev.off()
